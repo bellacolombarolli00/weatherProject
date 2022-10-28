e70020fb11d87b7e91205cfc6729b2deb495b1ab
bellacolombarolli00 <101637594+bellacolombarolli00@users.noreply.github.com>
2022-10-28 10:18:00 +0100

    weather icon added

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ app.get("/", function(req, res){
       const weatherData = JSON.parse(data)
       const temp = weatherData.main.temp
       const desc = weatherData.weather[0].description
-      const icon = weatherData.weather[0].icon 
+      const icon = weatherData.weather[0].icon
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png "
 
       res.write("<p>The weather is currently " + desc + "<p>");
       res.write ("<h1>The temperature in London is " + temp + " degrees Celcius.</h1>");
+      res.write ("<img src = "+ imageURL + ">");
       res.send()
     })
   })
